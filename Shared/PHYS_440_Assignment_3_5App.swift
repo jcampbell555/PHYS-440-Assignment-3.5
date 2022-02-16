a9ca622cff9f09c33e597cbd9297d91c714b95ce
Joshua Campbell <jcampbell1@hawk.iit.edu>
2022-02-16 16:42:31 -0600

    Initial Commit

---
+++ Shared/PHYS_440_Assignment_3_5App.swift
@@ -0,0 +1,17 @@
+//
+//  PHYS_440_Assignment_3_5App.swift
+//  Shared
+//
+//  Created by Joshua Campbell  on 2/16/22.
+//
+
+import SwiftUI
+
+@main
+struct PHYS_440_Assignment_3_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
